4d91ecd79c66ec72ebd91b17239ac74d5995bc18
DiyanTrifonov <diyantrifonov5@gmail.com>
2023-04-25 15:13:32 +0300

    First commit

---
+++ MaksimalnaPloshtadka/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaksimalnaPloshtadka
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Въведи ред: ");
+            int n = int.Parse(Console.ReadLine());
+            Console.Write("Въведи колона: ");
+            int m = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[n, n];
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] str = Console.ReadLine().Split();
+                for (int j = 0; j < m; j++)
+                {
+                    matrix[i, j] = int.Parse(str[j]);
+                }
+            }
+
+            int sumMax = int.MinValue;
+            int rowMax = -1, colMax = -1;
+
+            for (int row = 0; row < n - 1; row++)
+            {
+                for (int col = 0; col < m - 1; col++)
+                {
+                    int sum = matrix[row, col] + matrix[row, col + 1] + matrix[row + 1, col] + matrix[row + 1, col + 1];
+
+                    if (sum > sumMax)
+                    {
+                        sumMax = sum;
+                        rowMax = row;
+                        colMax = col;
+                    }
+                }
+            }
+
+            Console.WriteLine("Краен резултат:");
+            Console.WriteLine($"{matrix[rowMax, colMax]} {matrix[rowMax, colMax+1]}");
+            Console.WriteLine($"{matrix[rowMax+1,colMax]} {matrix[rowMax+1,colMax+1]}");
+        }
+    }
+}
---
+++ TriugulnikNaPaskal/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriugulnikNaPaskal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Въведи височина: ");
+            int h = int.Parse(Console.ReadLine());
+            int[][] triangle = new int[h + 1][];
+
+            for (int row = 0; row < h; row++)
+            {
+                triangle[row] = new int[row + 1];
+            }
+
+            triangle[0][0] = 1;
+
+            for (int row = 0; row < h - 1; row++)
+            {
+                for (int col = 0; col <= row; col++)
+                {
+                    triangle[row + 1][col] += triangle[row][col];
+                    triangle[row + 1][col + 1] += triangle[row][col];
+                }
+            }
+
+            for (int row = 0; row < h; row++)
+            {
+                for (int space = 0; space < h - row - 1; space++)
+                {
+                    Console.Write(" ");
+                }
+                for (int col = 0; col <= row; col++)
+                {
+                    Console.Write($"{triangle[row][col]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
